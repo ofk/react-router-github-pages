2035ffd06a0e2369ee32dd1ceed12e12cefd211a
ofk <ofkjpn+github@gmail.com>
2025-02-27 01:04:07 +0900

    Update react-router config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+import pkg from './package.json';
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? `/${pkg.name}/` : undefined,
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+}));
