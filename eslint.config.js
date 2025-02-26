d385f944a457c4002e72e4ffa2311522ff02aabe
ofk <ofkjpn+github@gmail.com>
2025-02-27 00:35:03 +0900

    Setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import config from '@ofk/eslint-config-recommend';
+
+export default config({
+  extends: [
+    {
+      files: ['app/{root,routes}.*', 'app/routes/**'],
+      rules: {
+        'react-refresh/only-export-components': 'off',
+      },
+    },
+  ],
+  ignores: ['.react-router/', 'build/'],
+  imports: {
+    defaultExportFiles: ['app/{root,routes}.*', 'app/routes/**'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+});
